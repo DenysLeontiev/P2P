add5926a4cca2e2cba47bd63cc0ea42b7c454e3d
Denys Leon <leonden0046@gmail.com>
2024-05-07 17:49:50 +0300

    Working video p2p call

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+let APP_ID = "6b20af0e82164e369d14f6cbc3a6618d";
+let token = null;
+
+let uid = String(Math.floor(Math.random() * 1000000));
+
+let client;
+let channel; // two users join here
+
 const constraints = {
     video: true,
     audio: false
@@ -16,17 +24,54 @@ let remoteStream;
 let peerConnection;
 
 let init = async () => {
+
+    client = await AgoraRTM.createInstance(APP_ID);
+    await client.login({ uid, token });
+
+    channel = client.createChannel('main'); // finds by name or creates new channel
+    await channel.join();
+
+    channel.on('MemberJoined', handleUserJoined);
+
+    client.on('MessageFromPeer', handleMessageFromPeer);
+
     localStream = await navigator.mediaDevices.getUserMedia(constraints);
     document.getElementById('local').srcObject = localStream;
-
-    createOffer();
 }
 
-let createOffer = async () => {
+let handleMessageFromPeer = async (message, memberId) => {
+    message = JSON.parse(message.text);
+    
+    if(message.type === 'offer') {
+        createAnswer(memberId, message.offer);
+    }
+
+    if(message.type === 'answer') {
+        addAnswer(message.answer);
+    }
+
+    if(message.type === 'candidate') {
+        if(peerConnection) {
+            peerConnection.addIceCandidate(message.candidate);
+        }
+    }
+}
+
+let handleUserJoined = async (memberId) => {
+    console.log("New user joined: ", memberId);
+    createOffer(memberId);
+}
+
+let createPeerConnection = async (memberId) => {
     peerConnection = new RTCPeerConnection(servers);
 
     remoteStream = new MediaStream();
     document.getElementById('remote').srcObject = remoteStream;
+
+    if(!localStream) {
+        localStream = await navigator.mediaDevices.getUserMedia(constraints);
+        document.getElementById('local').srcObject = localStream;
+    }
 
     localStream.getTracks().forEach((track) => {
         peerConnection.addTrack(track, localStream);
@@ -42,13 +87,37 @@ let createOffer = async () => {
 
     // called when setLocalDescription() is called
     peerConnection.onicecandidate = async (event) => {
-        if(event.candidate) {
+        if (event.candidate) {
             console.log(event.candidate);
+            client.sendMessageToPeer({ text: JSON.stringify({ 'type': 'candidate', 'candidate': event.candidate }) }, memberId);
         }
     }
+}
+
+let addAnswer = async (answer) => {
+    if(!peerConnection.currentRemoteDescription) {
+        peerConnection.setRemoteDescription(answer);
+    }
+}
+
+let createOffer = async (memberId) => {
+    await createPeerConnection(memberId);
 
     let offer = await peerConnection.createOffer();
     await peerConnection.setLocalDescription(offer);
+
+    client.sendMessageToPeer({ text: JSON.stringify({ 'type': 'offer', 'offer': offer }) }, memberId) // send to peer with this memberId
+}
+
+let createAnswer = async (memberId, offer) => {
+    await createPeerConnection(memberId);
+
+    await peerConnection.setRemoteDescription(offer);
+
+    let answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer);
+
+    client.sendMessageToPeer({text: JSON.stringify({'type':'answer','answer': answer})}, memberId);
 }
 
 init();
